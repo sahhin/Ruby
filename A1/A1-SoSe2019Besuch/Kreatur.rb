9c673a50fd558697e9e4f580af130ad2852c4de0
ezk0 <sahin.tekes@haw-hamburg.de>
2019-03-22 13:41:14 +0100

    A1 fertig + opt. Aufgaben

--- A1/A1-SoSe2019Besuch/Kreatur.rb
+++ A1/A1-SoSe2019Besuch/Kreatur.rb
@@ -1,17 +1,33 @@
 require "./Kreis"
 require "./Dreieck"
 require "./Rechteck"
-
+require "./Haus"
+require "./Point"
 class Kreatur
-  
   # TODO
   # Erzeugt eine Kreatur mit Hilfe der geometrischen Primitive auf Position x,y
   def initialize(x,y)
+    @x       = x
+    @y       = y
+    @hut     = Dreieck.new(x + 15, y - 110, 30, 30, "red", false)
+    @kopf    = Kreis.new(x + 15, y - 85, 15, "red", false )
+    @koerper = Kreis.new(x + 15, y - 30, 40, "red", false)
+    @arm_1   = Rechteck.new(x + 50, y - 30, 10,50, "red", false)
+    @arm_2   = Rechteck.new(x - 30, y - 30, 10, 50, "red", false)
+    @bein_1  = Rechteck.new(x + 25, y, 10, 40, "red", false)
+    @bein_2  = Rechteck.new(x, y, 10,40, "red",false)
 
   end
 
   # Macht die Kreatur sichtbar
   def sichtbar_machen()
+    @hut.sichtbar_machen()
+    @kopf.sichtbar_machen()
+    @koerper.sichtbar_machen()
+    @arm_1.sichtbar_machen()
+    @arm_2.sichtbar_machen()
+    @bein_1.sichtbar_machen()
+    @bein_2.sichtbar_machen()
   end
 
   # TODO
@@ -31,7 +47,13 @@ class Kreatur
   #   starten_nach - Zeitspanne ab der aktuellen Zeit, bis die gesamte Bewegung beginnt
   #
   def  bewegen(delta_x,delta_y,wdh,wdh_nach,starten_nach)
-
+    @hut.bewegen(delta_x, delta_y, wdh, wdh_nach, starten_nach)
+    @kopf.bewegen(delta_x, delta_y, wdh, wdh_nach, starten_nach)
+    @koerper.bewegen(delta_x, delta_y, wdh, wdh_nach, starten_nach)
+    @arm_1.bewegen(delta_x, delta_y, wdh, wdh_nach, starten_nach)
+    @arm_2.bewegen(delta_x, delta_y, wdh, wdh_nach, starten_nach)
+    @bein_1.bewegen(delta_x, delta_y, wdh, wdh_nach, starten_nach)
+    @bein_2.bewegen(delta_x, delta_y, wdh, wdh_nach, starten_nach)
   end
 
   # TODO 
@@ -41,7 +63,8 @@ class Kreatur
   # die "linke untere Ecke" des linken Fußes
   #
    def position()
-
+     punkt = Point.new(@bein_2.obere_linke_ecke.x, @bein_2.obere_linke_ecke.y+@bein_2.hoehe)
+     return punkt
    end
    
   # TODO
@@ -51,8 +74,8 @@ class Kreatur
   # Parameter
   #   ziel_pos_x - die x-Koordinate der neuen Position
   #   ziel_pos_y - die y-Koordinate der neuen Positione
-  def auf_position_setzen(ziel_pos_x,ziel_pos_y)
-
+  def auf_position(ziel_pos_x,ziel_pos_y)
+    bewegen(ziel_pos_x-position.x, ziel_pos_y-position.y, 1,  1, 0)
   end
 
   def to_s()
--- A1/A1-SoSe2019Besuch/Siedlung.rb
+++ A1/A1-SoSe2019Besuch/Siedlung.rb
@@ -7,17 +7,37 @@ class Siedlung
   # Eine Siedlung besteht aus 5 im Kreis angeordneten Häusern und einer Kreatur in der Mitte
   # der Siedlung
   def initialize()
+    @haus1 = Haus.new()
+    @haus2 = Haus.new()
+    @haus3 = Haus.new()
+    @haus4 = Haus.new()
+    @haus5 = Haus.new()
+    @kreatur = Kreatur.new(600, 500)
+
+    #auf die gewünschte Positionen setzen
+    @haus1.auf_position_setzen(500,200)
+    @haus2.auf_position_setzen(900,400)
+    @haus3.auf_position_setzen(1000, 800)
+    @haus4.auf_position_setzen(300,900)
+    @haus5.auf_position_setzen(150, 600)
+
 
   end
 
   # Stellt die Häuser und die Kreatur auf dem Bildschirm dar
   def sichtbar_machen()
-
+    @haus1.sichtbar_machen()
+    @haus2.sichtbar_machen()
+    @haus3.sichtbar_machen()
+    @haus4.sichtbar_machen()
+    @haus5.sichtbar_machen()
+    @kreatur.sichtbar_machen()
   end
 
   # Lässt die Kreatur eines der Häuser besuchen. Die Kreatur bewegt sich auf auf die Tür des Hauses zu und
   # bleibt vor dieser stehen
   def besuch()
+    @kreatur.auf_position(1000, 800)
 
   end
     
